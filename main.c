3a7c0538c63a187e38065a154a1d25ce48cb1117
ceskyDJ <6006391-ceskyDJ@users.noreply.gitlab.com>
2020-10-06 23:31:00 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/**
+ * IZP - projekt č. 1: Práce s textem
+ *
+ * @author Michal ŠMAHEL (xsmahe01)
+ * @date October 2020
+ * @copyright (C) Michal ŠMAHEL 2020
+ */
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+
+    // TODO: start working on the project...
+
+    return 0;
+}
