96dec12c0888a50f78c761a19af92aac134acf75
ceskyDJ <6006391-ceskyDJ@users.noreply.gitlab.com>
2020-10-09 21:15:31 +0200

    Test 18 - drow with R = 0

--- test/test.php
+++ test/test.php
@@ -177,6 +177,13 @@ $tester->createTest()
     ->addParams("-d ; irow -5")
     ->setFileInput($elmFunInput)
     ->setExpExitCode(1);
+// drow with R = 0
+$tester->createTest()
+    ->setName("drow with R = 0")
+    ->setScript($script)
+    ->addParams("-d ; drow 0")
+    ->setFileInput($elmFunInput)
+    ->setExpExitCode(1);
 
 $tester->runTests($successCallback, $failCallback);
 
