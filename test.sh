0046c2268c1ee7b0363fa1a3cc494c602501b5aa
ceskyDJ <6006391-ceskyDJ@users.noreply.gitlab.com>
2020-11-12 21:55:47 +0100

    Removed forgot changes in test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 # This file is for local tests (on GNU/Linux OSes) only
 
 # Compile C lang source codes
-gcc -std=c99 -Wall -Wextra -Werror ../sheet-dev/sheet.c -o tmp/sheet
+gcc -std=c99 -Wall -Wextra -Werror sheet.c -o tmp/sheet
 
 # Run PHP tests
 php test.php
